a10f45a5665bfaa4ab08a55322a0e2d8363f428a
Evgenii <aksyline@gmail.com>
2019-04-05 12:03:19 +0300

    add mutations to the store

--- src/store.js
+++ src/store.js
@@ -1,14 +1,61 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import uuid4 from 'uuid4'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    enteties: []
   },
   mutations: {
+    createEntity (state) {
+      state.enteties.push({
+        id: uuid4(),
+        name: 'New entity',
+        features: []
+      })
+    },
 
+    deleteEntity (state, payload) {
+      state.enteties = state.enteties.filter(e => e.id !== payload.id)
+    },
+
+    editEntity (state, payload) {
+      state.enteties = state.enteties.map(e => {
+        if (e.id !== payload.id) return e
+        return { ...e, ...payload.update }
+      })
+    },
+
+    createFeature (state, payload) {
+      state.enteties = state.enteties.map(e => {
+        if (e.id !== payload.entityId) return e
+        e.features.push({
+          id: uuid4(),
+          name: 'New feature'
+        })
+        return e
+      })
+    },
+
+    updateFeature (state, payload) {
+      state.enteties = state.enteties.map(e => {
+        if (e.id !== payload.entityId) return e
+        e.features = e.features.map(f => {
+          if (f.id !== payload.featureId) return f
+          f = { ...f, ...payload.update }
+          return f
+        })
+      })
+    },
+
+    deleteFeature (state, payload) {
+      state.enteties = state.enteties.map(e => {
+        if (e.id !== payload.entityId) return e
+        e.features = e.features.filter(f => f.id !== payload.featureId)
+      })
+    }
   },
   actions: {
 
